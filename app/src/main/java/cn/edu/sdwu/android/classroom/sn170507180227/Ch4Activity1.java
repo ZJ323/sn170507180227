e8909048eeb9bcedd57b002100a64fac6bf388d6
ZJ323 <1328741835@qq.com>
2020-04-02 17:46:54 +0800

    Android_课堂练习_20200402

--- app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180227/Ch4Activity1.java
+++ app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180227/Ch4Activity1.java
@@ -1,16 +1,23 @@
 package cn.edu.sdwu.android.classroom.sn170507180227;
 
 import android.app.WallpaperManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Ch4Activity1 extends AppCompatActivity {
+public class Ch4Activity1 extends AppCompatActivity implements View.OnFocusChangeListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +46,42 @@ public class Ch4Activity1 extends AppCompatActivity {
                 return true;
             }
         });
+
+        //事件源
+        EditText email=(EditText) findViewById(R.id.ch4_1_email);
+        email.setOnFocusChangeListener(this);
+
+        LinearLayout linearLayout=(LinearLayout)findViewById(R.id.ch4_1_ll);
+        linearLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                float x=motionEvent.getX();
+                float y=motionEvent.getY();
+                TextView textView=(TextView) findViewById(R.id.ch4_1_tv);
+                textView.setText("x:"+x+",y:"+y);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onFocusChange(View view, boolean b) {
+        //参数b代表是否获取焦点
+        //判断邮箱地址的合法性
+        EditText editText=(EditText)view;
+        if (!b){
+            String content=editText.getText().toString();//得到EditText的内容
+            //判断邮箱地址的正则表达式
+            String regExl="^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+            Pattern pattern=Pattern.compile(regExl);
+            Matcher matcher=pattern.matcher(content);
+            TextView textView=(TextView)findViewById(R.id.ch4_1_tv);
+            if (matcher.matches()){
+                textView.setText("");
+            }else {
+                textView.setText("email invalidate");
+            }
+        }
     }
 
     //2)实现时间监听类，该监听类是一个特殊的java类，必须实现一个XXXListener接口；
@@ -47,5 +90,11 @@ public class Ch4Activity1 extends AppCompatActivity {
         public void onClick(View view) {
             Log.i(Ch4Activity1.class.toString(),"button click");
         }
+    }
+
+    public void startMain(View view){
+        //界面跳转
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
