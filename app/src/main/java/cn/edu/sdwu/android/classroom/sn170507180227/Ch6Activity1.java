5bea16e72a8c9f3044a83072e42d445ffe2f1461
ZJ323 <1328741835@qq.com>
2020-04-08 16:13:39 +0800

    Android_课堂练习_20200408

--- app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180227/Ch6Activity1.java
+++ app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180227/Ch6Activity1.java
@@ -1,8 +1,18 @@
 package cn.edu.sdwu.android.classroom.sn170507180227;
 
+import android.content.res.Resources;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class Ch6Activity1 extends AppCompatActivity {
 
@@ -19,5 +29,82 @@ public class Ch6Activity1 extends AppCompatActivity {
         String s=String.format(sms,100,"张三");
         Log.i(Ch6Activity1.this.toString(),s);
 
+        //获取数组资源
+        Resources resources=getResources();
+        int[] intArr=resources.getIntArray(R.array.intArr);
+        for (int i=0;i<intArr.length;i++){
+            Log.i(Ch6Activity1.this.toString(),intArr[i]+"");
+        }
+        //字符串数组
+        String[] strArr=resources.getStringArray(R.array.strArr);
+        for (int i=0;i<strArr.length;i++){
+            Log.i(Ch6Activity1.this.toString(),strArr[i]);
+        }
+
+        //普通类型数组
+        TypedArray typedArray=resources.obtainTypedArray(R.array.commanArr);
+        ImageView imageView=(ImageView)findViewById(R.id.ch6_1_iv);
+        int resId=typedArray.getResourceId(0,0);
+        imageView.setImageResource(resId);
+
+        String str=typedArray.getString(1);
+        Log.i(Ch6Activity1.this.toString(),str);
+
+        //注册上下文菜单
+        LinearLayout linearLayout=(LinearLayout)findViewById(R.id.ch6_1_ll);
+        registerForContextMenu(linearLayout);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        //加载自定义的菜单资源
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.mymenu1,menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.mymenu_item1:
+                Toast.makeText(this,"item1",Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.mymenu_item2:
+                Toast.makeText(this,"item2",Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.mymenu_item3:
+                Toast.makeText(this,"item3",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //基于回调的事件处理
+        //根据菜单项的ID进行区分
+        switch(item.getItemId()){
+            case R.id.mymenu_item1:
+                Toast.makeText(this,"item1",Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.mymenu_item2:
+                Toast.makeText(this,"item2",Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.mymenu_item3:
+                Toast.makeText(this,"item3",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        //加载自定义的菜单资源
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.mymenu1,menu);
+        return true;
     }
 }
